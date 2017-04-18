fa4362bcfbc2c12bb2338ca5e36dcf5e00a76db8
Godwin <635682185@qq.com>
2017-04-18 11:29:43 +0800

    test

--- Algorithms/Sort/Sort.cpp
+++ Algorithms/Sort/Sort.cpp
@@ -1,6 +1,24 @@
 #include "Sort.h"
 #include "Bubbling.h"
 
+List::List()
+{
+	length = 0;
+	list = (int *)malloc(sizeof(int)* SIZE);
+	size = SIZE;
+}
+void List::insert(int data)
+{
+	if (length < size)
+		list[length++] = data;
+	else
+	{
+		list = (int *)realloc(list, sizeof(int)* SIZE);
+		list[length++] = data;
+		size += SIZE;
+	}
+}
+
 int main() {
 
 	return 0;
